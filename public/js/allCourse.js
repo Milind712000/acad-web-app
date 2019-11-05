4989fb0a6d17a24e2c8ef18899c1c70e8780ad4a
Milind Topno <email2milindtopno.test@gmail.com>
2019-11-06 00:18:17 +0530

    improve tables

--- public/js/allCourse.js
+++ public/js/allCourse.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
+
 $(document).ready(function() {
-	$('#example').DataTable({
-		// "paging":   false,
-        // "ordering": false,
-        // "info":     false
-	});
+	const table = $('.dataTable').DataTable({});
+
+	table.on( 'order.dt search.dt', function () {
+		table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+			cell.innerHTML = i+1;
+		} );
+	} ).draw();
 });
